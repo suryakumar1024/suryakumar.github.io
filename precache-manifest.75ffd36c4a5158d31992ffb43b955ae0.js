e207bdb08d8d3d625558340cc507e9137d11fcf7
SURYA KUMAR K <k.suryakumar1024@gmail.com>
2020-08-26 12:59:25 +0530

    Init profile
    Updated probile

---
+++ precache-manifest.75ffd36c4a5158d31992ffb43b955ae0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c17b2dc2ca016981b33a04757a18ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ecd66ef467d3edc5f8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84b292c93dd070b7815e",
+    "url": "/static/js/2.6b37afcd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6b37afcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ecd66ef467d3edc5f8",
+    "url": "/static/js/main.7b89e480.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "9a93f99cc6b6a143a328b8565c1247a5",
+    "url": "/static/media/background.9a93f99c.png"
+  },
+  {
+    "revision": "a76c8cf6c84bec2fc9c8c3cfc6599d4b",
+    "url": "/static/media/profile.a76c8cf6.jpg"
+  }
+]);
